d1b192a3be191a2e1c1c46a3f3b0887ef4def672
Jessica Bueno <buenojessicaaparecida@gmail.com>
2026-03-16 23:15:50 -0300

    add hello.py

---
+++ hello.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Esse programa imprime uma mensagem de boas-vindas."""
+
+__version__ = "0.0.1"
+__author__ = "Jessica Bueno"
+__license__ = "Unlicense"
+
+name = "Jessica"
+age = 36
+pinguim = r"""
+     .--.       
+    |o_o |      OK, tchau!
+    |:_/ |    /
+   //   \ \  o
+  (|     | )
+ /'\_   _/`\
+ \___)=(___/
+"""
+
+message = f"Hello, my name is {name} and I am {age} years old."
+
+print(message)
+print(pinguim)
